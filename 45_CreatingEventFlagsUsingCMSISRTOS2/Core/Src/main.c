242e6882a6764adf8e4550256feb941144ea40c0
georgecatalin <george.calin@gmail.com>
2023-08-10 12:44:41 +0300

    Understand EventFlags with CMSIS RTOS2 API

---
+++ 45_CreatingEventFlagsUsingCMSISRTOS2/Core/Src/main.c
@@ -0,0 +1,350 @@
+/* https://www.keil.com/pack/doc/CMSIS/RTOS2/html/group__CMSIS__RTOS__EventFlags.html*/
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+#include <stdbool.h>
+#include "uart.h"
+#include <stdio.h>
+
+
+typedef uint32_t TaskProfiler;
+
+TaskProfiler OrangeTaskProfiler, WhiteTaskProfiler, BlackTaskProfiler, RedTaskProfiler;
+TaskProfiler timer1_profiler, timer2_profiler;
+
+#define STACK_SIZE 128 // In CMSIS RTOS2 STACK SIZE IS MEASURED IN BYTES
+
+osThreadId_t orange_id, white_id, black_id, red_id;
+
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+
+void orangeTask(void *pvParams);
+void whiteTask(void *pvParams);
+void blackTask(void *pvParams);
+void redTask(void *pvParams);
+
+osTimerId_t timer1_id, timer2_id;
+
+void timer1_callback_function(void *args);
+void timer2_callback_function(void *args);
+
+osEventFlagsId_t eventFlags_1 = NULL;
+
+
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  USART3_UART_TX_Init();
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* Create the thread(s) */
+
+  //Create Threads
+  osThreadAttr_t  orange_attributes = { .name="Orange Task", .priority=osPriorityNormal, .stack_size = STACK_SIZE};
+  orange_id= osThreadNew(orangeTask, NULL, &orange_attributes);
+
+  osThreadAttr_t white_attributes =  { .name="White Task", .priority=osPriorityNormal1, .stack_size = STACK_SIZE};
+  white_id=osThreadNew(whiteTask, NULL, &white_attributes);
+
+  osThreadAttr_t black_attributes =  { .name="Black Task", .priority=osPriorityNormal2, .stack_size = STACK_SIZE};
+  black_id=osThreadNew(blackTask, NULL, &black_attributes);
+
+  osThreadAttr_t red_attributes =  { .name="Red Task", .priority=osPriorityNormal3, .stack_size = STACK_SIZE};
+  red_id=osThreadNew(redTask, NULL, &red_attributes);
+
+  //Create timers
+  timer1_id = osTimerNew(timer1_callback_function, osTimerOnce, NULL, NULL);
+  timer2_id = osTimerNew(timer2_callback_function, osTimerPeriodic, NULL, NULL);
+
+  //Create EventFlag objec
+  const osEventFlagsAttr_t event_attributes = { .name="attributes"};
+  eventFlags_1 = osEventFlagsNew(&event_attributes);
+
+  /* Start scheduler */
+  osKernelStart();
+
+
+  while (1)
+  {
+
+  }
+
+}
+
+
+void orangeTask(void *pvParams)
+{
+
+
+
+	while(1)
+	{
+		OrangeTaskProfiler++;
+
+		//The function osEventFlagsSet sets the event flags specified by the parameter flags in an event flags object specified by parameter ef_id.
+        osEventFlagsSet(eventFlags_1, 0x01);
+
+		osDelay(100);
+	}
+}
+void whiteTask(void *pvParams)
+{
+
+	while(1)
+	{
+		WhiteTaskProfiler++;
+
+        osEventFlagsWait(eventFlags_1, 0x01, osFlagsWaitAny, osWaitForever);
+        printf("Running this in the White Task after receiving the flag 0x01 \n\r");
+
+		osDelay(100);
+	}
+}
+
+void blackTask(void *pvParams)
+{
+	osTimerStart(timer1_id, 500);
+	osTimerStart(timer2_id, 100);
+
+	while(1)
+	{
+		BlackTaskProfiler++;
+
+
+		osDelay(100);
+	}
+}
+
+void redTask(void *pvParams)
+{
+	while(1)
+	{
+		RedTaskProfiler++;
+
+
+
+		osDelay(100);
+	}
+}
+
+void timer1_callback_function(void *args)
+{
+	timer1_profiler++;
+}
+
+
+void timer2_callback_function(void *args)
+{
+	timer2_profiler++;
+}
+
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART3 Initialization Function
+  * @param None
+  * @retval None
+  */
+
+
+static void MX_GPIO_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    HAL_GPIO_WritePin(GPIOB, LD1_Pin|LD3_Pin|LD2_Pin, GPIO_PIN_RESET);
+
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(USB_PowerSwitchOn_GPIO_Port, USB_PowerSwitchOn_Pin, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin : USER_Btn_Pin */
+    GPIO_InitStruct.Pin = USER_Btn_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : RMII_MDC_Pin RMII_RXD0_Pin RMII_RXD1_Pin */
+    GPIO_InitStruct.Pin = RMII_MDC_Pin|RMII_RXD0_Pin|RMII_RXD1_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : RMII_REF_CLK_Pin RMII_MDIO_Pin RMII_CRS_DV_Pin */
+    GPIO_InitStruct.Pin = RMII_REF_CLK_Pin|RMII_MDIO_Pin|RMII_CRS_DV_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : LD1_Pin LD3_Pin LD2_Pin */
+    GPIO_InitStruct.Pin = LD1_Pin|LD3_Pin|LD2_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : RMII_TXD1_Pin */
+    GPIO_InitStruct.Pin = RMII_TXD1_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+    HAL_GPIO_Init(RMII_TXD1_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : USB_PowerSwitchOn_Pin */
+    GPIO_InitStruct.Pin = USB_PowerSwitchOn_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(USB_PowerSwitchOn_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : USB_OverCurrent_Pin */
+    GPIO_InitStruct.Pin = USB_OverCurrent_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(USB_OverCurrent_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : USB_SOF_Pin USB_ID_Pin USB_DM_Pin USB_DP_Pin */
+    GPIO_InitStruct.Pin = USB_SOF_Pin|USB_ID_Pin|USB_DM_Pin|USB_DP_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF10_OTG_FS;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : USB_VBUS_Pin */
+    GPIO_InitStruct.Pin = USB_VBUS_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(USB_VBUS_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : RMII_TX_EN_Pin RMII_TXD0_Pin */
+    GPIO_InitStruct.Pin = RMII_TX_EN_Pin|RMII_TXD0_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
+    HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+
+}
+
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
